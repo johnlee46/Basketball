d073a1f5b0b088ba05e06e58cd1a04607554fcfc
John <johnlee46@gmail.com>
2020-10-29 19:25:37 -0700

    initial commit
    initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    public GameObject ball;
+    public GameObject playerCamera;
+
+    private bool holdingBall = true;
+    public float force = 2000f;
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holdingBall)
+        {
+            ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * 4;
+            if (Input.GetMouseButtonDown(0))
+            {
+                holdingBall = false;
+                ball.GetComponent<Rigidbody>().useGravity = true;
+                //ball.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * force);
+            }
+        }
+
+    }
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+
+    //when player release the mouse to flick or / touch if mobile
+    void OnMouseUp()
+    {
+
+    }
+
+}
