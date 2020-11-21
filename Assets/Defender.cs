94dd6c74b26ed9757b2b9c36fb74231ca2174790
John <johnlee46@gmail.com>
2020-11-20 23:11:52 -0800

    added an oscillating defender
    added an oscillating defender

---
+++ Assets/Defender.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour {
+
+    // Use this for initialization
+    //public GameObject target;
+    public float speed = 1.19f;
+    Vector3 pointA;
+    Vector3 pointB;
+    void Start () {
+        pointA = new Vector3(-5, 0, 7);
+        pointB = new Vector3(5,0 , 7);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject target = GameObject.FindGameObjectWithTag("ball");
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+
+    }
+}
